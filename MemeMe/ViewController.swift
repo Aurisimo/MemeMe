4a6f5a60397392559ed21f4aa281dcf862b7dbb5
aurelijuslape <aurelijus_lape@yahoo.com>
2019-09-27 15:53:26 +0300

    Initial Commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Aurelijus Lape on 27/09/2019.
+//  Copyright © 2019 Aurelijus Lape. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
